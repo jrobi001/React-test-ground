839a8529bfd6fe3e73cf354890ffad4ad7f3aceb
jrobi001 <jasper.robinson93@gmail.com>
2021-10-22 09:48:49 +0100

    finished this part

--- my-app/src/components/CourseGoals/CourseInput/CourseInput.js
+++ my-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,34 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // don't accept empty inputs
+    if (enteredValue.trim().length === 0) {
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} required='true' pattern=".*\S+.*" title="This field is required" />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button isReady={isValid} type="submit" >Add Goal</Button>
     </form>
   );
 };
--- my-app/src/components/UI/Button/Button.js
+++ my-app/src/components/UI/Button/Button.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-import './Button.css';
+// import './Button.css'
+import styles from './Button.module.css'
+// import cx from classnames
+
 
 const Button = props => {
+
+  const buttonStyling = props.isReady ? styles.button : [styles.button, styles.invalid].join(' ');
+
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={buttonStyling} onClick={props.onClick}>
       {props.children}
     </button>
   );
